4dc0a82d0e9415358a55b1fc9a6b7188e6d2417a
Martín Mongi Badía <martinmongi@gmail.com>
2017-10-10 00:01:45 -0300

    Ejercicio 5 con cut, revisar

--- tp_logica/batalla_naval.pl
+++ tp_logica/batalla_naval.pl
@@ -54,7 +54,15 @@ hayBarco(N, horizontal, T, F, C) :- contenido(T, F, C, o), N1 is N - 1, C1 is C 
 ubicarBarcos([], T).
 ubicarBarcos([B|Bs], T) :- puedoColocar(B, Dir, T, F, C), hayBarco(B, Dir, T, F, C), ubicarBarcos(Bs, T).
 
+% completarCasilleroConAgua(+Casillero, ?Resultado)
+completarCasilleroConAgua(X, ~) :- \+ atom(X),!.
+completarCasilleroConAgua(o, o).
+
+%completarConAgua(+Original, ?Completo)
+completarFilaConAgua(X, Y) :- maplist(completarCasilleroConAgua, X, Y).
+
 %completarConAgua(+?Tablero)
+completarConAgua(X, Y) :- maplist(completarFilaConAgua, X, Y).
 
 %golpear(+Tablero, +NumFila, +NumColumna, -NuevoTab)
 
