b8359bcb9111edb4a1f31639999270027a7bfc97
Francisco Demartino <demartino.francisco@gmail.com>
2017-10-10 00:25:34 -0300

    singleton nit

--- tp_logica/batalla_naval.pl
+++ tp_logica/batalla_naval.pl
@@ -54,7 +54,7 @@ hayBarco(N, vertical, T, F, C) :- contenido(T, F, C, o), N1 is N - 1, F1 is F + 
 hayBarco(N, horizontal, T, F, C) :- contenido(T, F, C, o), N1 is N - 1, C1 is C + 1, hayBarco(N1, horizontal, T, F, C1).
 
 %ubicarBarcos(+Barcos, +?Tablero)
-ubicarBarcos([], T).
+ubicarBarcos([], _).
 ubicarBarcos([B|Bs], T) :- puedoColocar(B, Dir, T, F, C), hayBarco(B, Dir, T, F, C), ubicarBarcos(Bs, T).
 
 %completarCasilleroConAgua(+Casillero, ?Resultado)
