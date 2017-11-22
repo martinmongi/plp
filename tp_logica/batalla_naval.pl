d2ff0fbcf544bffbd9f571f1d9787436479ffd8f
Martín Mongi Badía <martinmongi@gmail.com>
2017-11-22 16:35:09 -0300

    Rename

--- tp_logica/batalla_naval.pl
+++ tp_logica/batalla_naval.pl
@@ -41,21 +41,33 @@ contenido(T, F, C, Cont) :- nth1(F, T, Fila), nth1(C, Fila, Cont).
 vacio(T, F, C) :- contenido(T, F, C, Cont), var(Cont).
 
 %disponible(+Tablero, ?Fila, ?Columna)
-disponible(T, F, C) :- vacio(T, F, C), forall(adyacenteEnRango(T, F, C, F_1, C_1), vacio(T, F_1, C_1)).
+disponible(T, F, C) :- vacio(T, F, C),
+                       forall(adyacenteEnRango(T, F, C, F_1, C_1),
+                       vacio(T, F_1, C_1)).
 
 %puedoColocar(+CantPiezas, ?Direccion, +Tablero, ?Fila, ?Columna)
 puedoColocar(1, _, T, F, C) :- disponible(T, F, C).
-puedoColocar(N, vertical, T, F, C) :- disponible(T, F, C), N1 is N - 1, F1 is F + 1, puedoColocar(N1, vertical, T, F1, C).
-puedoColocar(N, horizontal, T, F, C) :- disponible(T, F, C), N1 is N - 1, C1 is C + 1, puedoColocar(N1, horizontal, T, F, C1).
+puedoColocar(N, vertical, T, F, C) :- disponible(T, F, C),
+                                      N1 is N - 1, F1 is F + 1,
+                                      puedoColocar(N1, vertical, T, F1, C).
+puedoColocar(N, horizontal, T, F, C) :- disponible(T, F, C),
+                                        N1 is N - 1, C1 is C + 1,
+                                        puedoColocar(N1, horizontal, T, F, C1).
 
 %hayBarco(+CantPiezas, ?Direccion, +Tablero, ?Fila, ?Columna)
 hayBarco(1, _, T, F, C) :- contenido(T, F, C, o).
-hayBarco(N, vertical, T, F, C) :- contenido(T, F, C, o), N1 is N - 1, F1 is F + 1, hayBarco(N1, vertical, T, F1, C).
-hayBarco(N, horizontal, T, F, C) :- contenido(T, F, C, o), N1 is N - 1, C1 is C + 1, hayBarco(N1, horizontal, T, F, C1).
+hayBarco(N, vertical, T, F, C) :- contenido(T, F, C, o),
+                                  N1 is N - 1, F1 is F + 1,
+                                  hayBarco(N1, vertical, T, F1, C).
+hayBarco(N, horizontal, T, F, C) :- contenido(T, F, C, o),
+                                    N1 is N - 1, C1 is C + 1,
+                                    hayBarco(N1, horizontal, T, F, C1).
 
 %ubicarBarcos(+Barcos, +?Tablero)
 ubicarBarcos([], _).
-ubicarBarcos([B|Bs], T) :- puedoColocar(B, Dir, T, F, C), hayBarco(B, Dir, T, F, C), ubicarBarcos(Bs, T).
+ubicarBarcos([B|Bs], T) :- puedoColocar(B, Dir, T, F, C),
+                           hayBarco(B, Dir, T, F, C),
+                           ubicarBarcos(Bs, T).
 
 %completarConAgua(+?Tablero)
 completarConAgua(T) :- maplist(completarFilaConAgua, T).
@@ -70,7 +82,8 @@ completarCasilleroConAgua(o).
 %reemplazar(+Lista, +Indice, +Elemento, -Resultado)
 reemplazar([], _, _, []).
 reemplazar([_|Xs], 1, Z, [Z|Xs]).
-reemplazar([X|Xs], N, Z, [X|Ys]) :- N > 1, N1 is N - 1, reemplazar(Xs, N1, Z, Ys).
+reemplazar([X|Xs], N, Z, [X|Ys]) :- N > 1, N1 is N - 1,
+                                    reemplazar(Xs, N1, Z, Ys).
 
 %golpear(+Tablero, +NumFila, +NumColumna, -NuevoTab)
 golpear(T, F, C, T) :- contenido(T, F, C, ~).
@@ -81,10 +94,14 @@ golpear(T, F, C, NuevoT) :- contenido(T, F, C, o), nth1(F, T, Fila),
 % Completar instanciación soportada y justificar.
 %atacar(+Tablero, ?Fila, ?Columna, ?Resultado, ?NuevoTab)
 atacar(T, F, C, agua, T) :- golpear(T, F, C, T).
-atacar(T, F, C, hundido, NuevoT) :- contenido(T, F, C, o), golpear(T, F, C, NuevoT),
-                                    forall(adyacenteEnRango(T, F, C, F_1, C_1), contenido(T, F_1, C_1, ~)).
-atacar(T, F, C, tocado, NuevoT) :- contenido(T, F, C, o), golpear(T, F, C, NuevoT),
-                                   \+ forall(adyacenteEnRango(T, F, C, F_1, C_1), contenido(T, F_1, C_1, ~)).
+atacar(T, F, C, hundido, NuevoT) :- contenido(T, F, C, o),
+                                    golpear(T, F, C, NuevoT),
+                                    forall(adyacenteEnRango(T, F, C, F_1, C_1),
+                                    contenido(T, F_1, C_1, ~)).
+atacar(T, F, C, tocado, NuevoT) :- contenido(T, F, C, o),
+                                   golpear(T, F, C, NuevoT),
+                                   adyacenteEnRango(T, F, C, F_1, C_1),
+                                   contenido(T, F_1, C_1, o).
 
 %------------------Tests:------------------%
 
@@ -148,9 +165,9 @@ test(9) :-
   contenido(NuevaM,1,1,~),
   contenido(NuevaM,1,2,~).
 
-% No funciona, debe haber algun problema con la sintaxis
 test(10) :-
   Tablero = [[o, o], [_, _], [_, o]],
   completarConAgua(Tablero) = [[o, o], [~, ~], [~, o]].
 
-tests :- forall(between(1, 9, N), test(N)). % Cambiar el 2 por la cantidad de tests que tengan.
+% Cambiar el 2 por la cantidad de tests que tengan.
+tests :- forall(between(1, 10, N), test(N)). 
